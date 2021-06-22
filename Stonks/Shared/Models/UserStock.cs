ef662c0b268325b9521ed31f100d2cf00f3e561a
Valentyn Saik <svbackend22@gmail.com>
2021-06-22 09:21:34 +0300

    Added UserStock model. need to add idStock

---
+++ Stonks/Shared/Models/UserStock.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+using Microsoft.EntityFrameworkCore;
+
+namespace Stonks.Shared.Models
+{
+    public class UserStock
+    {
+        [Required]
+        [ForeignKey("IdUser")]
+        public ApplicationUser User { get; set; }
+
+        public Guid IdUser { get; set; }
+        
+        [Required]
+        [ForeignKey("IdStock")]
+        public ApplicationUser Stock { get; set; } // todo add IdStock
+
+        public int IdStock { get; set; }
+    }
+}
